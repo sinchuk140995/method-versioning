0cc99f7033836c2686d2e5d62366ba843be4c218
Bohdan Sinchuk <sinchuk140995@gmail.com>
2019-01-20 23:20:09 +0200

    versioning decorator: add empty line before @wrap

--- method_versioning/decorator.py
+++ method_versioning/decorator.py
@@ -4,13 +4,13 @@ from functools import wraps
 def versioning(version, name=''):
 
     def versioning_decorator(function):
+
         @wraps(function)
         def wrapper(*args, **kwargs):
             return function(*args, **kwargs)
 
         setattr(wrapper, 'mv_name', name if name else function.__name__)
         setattr(wrapper, 'mv_version', version)
-
         return wrapper
 
     return versioning_decorator
